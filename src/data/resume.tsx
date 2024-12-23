7794a9a3b17e389edc6519a1bdf22d0bca397f35
Sanjay1S <sanjaymes011@gmail.com>
2024-12-22 23:07:21 -0800

    fix-name

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
+  name: "Sanjay Srinivasa",
   initials: "DV",
   url: "https://sanjay1s.github.io",
   location: "San Francisco, CA",
